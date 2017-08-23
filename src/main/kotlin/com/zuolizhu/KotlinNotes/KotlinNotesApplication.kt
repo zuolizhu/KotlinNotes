3462119dc475eeca09478405535a6dee4b727d93
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-22 21:07:19 -0400

    constructor injection

--- src/main/kotlin/com/zuolizhu/KotlinNotes/KotlinNotesApplication.kt
+++ src/main/kotlin/com/zuolizhu/KotlinNotes/KotlinNotesApplication.kt
@@ -1,10 +1,13 @@
 package com.zuolizhu.KotlinNotes
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.annotation.Id
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.stereotype.Component
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 
@@ -21,3 +24,14 @@ data class Note(@Id @GeneratedValue var id: Long? = null,
 
 @RepositoryRestResource
 interface NotesRepository : JpaRepository<Note, Long>
+
+@Component
+class DataInitializer(val repository: NotesRepository) : ApplicationRunner {
+    @Throws(Exception::class)
+    override fun run(args: ApplicationArguments) {
+        listOf("Note 1", "Notes 2", "Note 3").forEach {
+            repository.save(Note(text = it, user = "user"))
+        }
+        repository.findAll().forEach {println(it)}
+    }
+}
