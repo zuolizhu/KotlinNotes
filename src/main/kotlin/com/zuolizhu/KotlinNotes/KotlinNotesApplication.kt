2710b43e3213d8935812d70bf654712bf7e49e54
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-22 21:03:16 -0400

    Entity added
    Type? = null means arguments are nullable when creating a new instance of the class

--- src/main/kotlin/com/zuolizhu/KotlinNotes/KotlinNotesApplication.kt
+++ src/main/kotlin/com/zuolizhu/KotlinNotes/KotlinNotesApplication.kt
@@ -2,6 +2,9 @@ package com.zuolizhu.KotlinNotes
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.annotation.Id
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 
 @SpringBootApplication
 class KotlinNotesApplication
@@ -9,3 +12,7 @@ class KotlinNotesApplication
 fun main(args: Array<String>) {
     SpringApplication.run(KotlinNotesApplication::class.java, *args)
 }
+
+@Entity
+data class Note(@Id @GeneratedValue var id: Long? = null,
+                var text: String? = null, var user: String? = null)
